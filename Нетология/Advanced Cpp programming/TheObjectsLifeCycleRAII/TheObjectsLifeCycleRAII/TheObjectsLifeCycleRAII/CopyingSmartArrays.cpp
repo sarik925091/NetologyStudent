9402812681e0d8ee2b528cf5155ec0ecef999d08
sardor majidov <sarik925091@gmail.com>
2026-01-18 18:24:34 +0300

    module5: task2

---
+++ Нетология/Advanced Cpp programming/TheObjectsLifeCycleRAII/TheObjectsLifeCycleRAII/TheObjectsLifeCycleRAII/CopyingSmartArrays.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<exception>
+
+class smart_array
+{
+private:
+	int* arr;
+	int size;
+public:
+	smart_array(int size)
+	{
+		arr = new int[size] {};
+		this->size = size;
+	}
+	void add_element(int n)
+	{
+		if (arr[size - 1] != 0)
+		{
+			throw std::exception("Index outside the array, the array is overflowing!");
+		}
+		else
+		{
+			for (size_t i = 0; i < size; ++i)
+			{
+				if (arr[i] == 0)
+				{
+					arr[i] = n;
+					break;
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+	}
+	int get_element(int i)
+	{
+		if (i >= size)
+		{
+			throw std::exception("Index outside the array!");
+		}
+		else
+		{
+			return arr[i];
+		}
+	}
+
+	smart_array& operator=(const smart_array& other) 
+	{
+		if (this != &other) 
+		{ 
+			delete[] arr; 
+
+			size = other.size;
+			arr = new int[size];
+
+			for (size_t i = 0; i < size; ++i)
+			{
+				arr[i] = other.arr[i];
+			}
+		}
+		return *this;
+	}
+
+	~smart_array()
+	{
+		delete[] arr;
+	}
+};
+
+int main()
+{
+	try {
+		smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+
+		smart_array new_array(2);
+		new_array.add_element(44);
+		new_array.add_element(34);
+
+		arr = new_array;
+		std::cout << new_array.get_element(0) << std::endl;
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+	return 0;
+}
---
+++ Нетология/Advanced Cpp programming/TheObjectsLifeCycleRAII/TheObjectsLifeCycleRAII/TheObjectsLifeCycleRAII/SmartArray.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<exception>
+class smart_array
+{
+private:
+	int* arr;
+	int size;
+public:
+	smart_array(int size)
+	{
+		arr = new int[size] {};
+		this->size = size;
+	}
+	void add_element(int n)
+	{
+		if (arr[size - 1] != 0)
+		{
+			throw std::exception("Index outside the array, the array is overflowing!");
+		}
+		else
+		{
+			for (size_t i = 0; i < size; ++i)
+			{
+				if (arr[i] == 0)
+				{
+					arr[i] = n;
+					break;
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+	}
+	int get_element(int i)
+	{
+		if (i >= size)
+		{
+			throw std::exception("Index outside the array!");
+		}
+		else
+		{
+			return arr[i];
+		}
+	}
+	~smart_array()
+	{
+		delete[] arr;
+	}
+};
+
+int main()
+{
+	try 
+	{
+		smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+		arr.add_element(14);
+		arr.add_element(15);
+		std::cout << arr.get_element(0) << std::endl;
+	}
+	catch (const std::exception& ex) 
+	{
+		std::cout << ex.what() << std::endl;
+	}
+	return 0;
+}
