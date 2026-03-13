ca3c502bd4774a3d9f53dfc741863195a0166606
sardor majidov <sarik925091@gmail.com>
2026-03-13 17:50:06 +0300

    module6: task1

---
+++ Нетология/Working with databases/TypesOfDatabases/Script.sql
@@ -0,0 +1,24 @@
+create table if not exists genres (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL unique   
+);
+
+create table if not exists artists (
+	id serial primary key,
+	name varchar(200) not null,
+	genres_id INTEGER not null references genres(id) on delete restrict
+);
+
+create table if not exists album (
+	id serial primary key,
+	title varchar(200) not null,
+	release_year integer check (release_year > 1900),
+	arists_id integer not null references artists(id) ON DELETE CASCADE
+);
+
+create table if not exists track (
+	id serial primary key,
+	title varchar(200) not null,
+	duration  integer not null,
+	album_id integer not null references artists(id) ON DELETE CASCADE
+);
